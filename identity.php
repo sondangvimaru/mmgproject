3f05a58bf0e142685252d4fc5414877f16dfc737
sondangvimaru <dangson28121998@gmail.com>
2021-11-02 16:31:19 +0700

    update

--- identity.php
+++ identity.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Xác Thực Danh Tính</title>
 </head>
 <body>
     
-    <script>
-        window.location = 'intent:https://google.com#Intent;end';
+<script>
+    var url = "https://magic.veriff.me/v/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uX2lkIjoiNDM3YWZlZWUtMzcwMy00ODRjLTkxODEtMDhjZWM4N2NmYTVhIiwiaWF0IjoxNjM1Nzk2ODIzfQ.AkUxNOsN4yisgtKF2Nm48OJnOHpbL7pWS3PYd3CBFM0?fallback_from=qr-code&lang=vi&fbclid=IwAR3rz6Ur7uqbLieEptd5ydsf8y7xq6i6gbHmqZ_26W8c5ZlfeSFVjqCvwno"
+   url= url.replace(/(^\w+:|^)\/\//, '');
+    window.location.href="intent://"+url+"#Intent;scheme=https;package=com.android.chrome;end"
     </script>
 </body>
 </html>
